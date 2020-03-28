13ad6168f65c90d52147452a628708835e320f11
Marek Ciążyński <m.ciazynski@bfirst.tech>
2020-03-28 18:57:19 +0100

    Sample map set to correct location

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 'use strict';
 import './style.css';
-// import 'leaflet/dist/images/marker-icon-2x.png';
-// import 'leaflet/dist/images/marker-shadow.png';
 
-import './node_modules/leaflet/dist/leaflet.css';
-import 'leaflet';
-
-// console.log('Hello World');
-// var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-// L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-// }).addTo(mymap);
+// imported in index.html
+// import './node_modules/leaflet/dist/leaflet.css';
+// import 'leaflet';
+// import 'leaflet-draw';
+// import 'leaflet-streetlabels';
 
 /* ----------------- */
 function init() {
-	var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-		osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-		osm = L.tileLayer(osmUrl, { maxZoom: 18, attribution: osmAttrib }),
-		map = new L.Map('map', { center: new L.LatLng(51.505, -0.04), zoom: 13 }),
-		drawnItems = L.featureGroup().addTo(map);
+	const osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	const osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+	const osm = L.tileLayer(osmUrl, { maxZoom: 18, attribution: osmAttrib });
+	const map = new L.Map('map', {
+		center: new L.LatLng(51.509, -0.135),
+		zoom: 13.6,
+		// zoomDelta: 0.25,
+		zoomSnap: 0
+	});
+	const drawnItems = L.featureGroup().addTo(map);
+
+	// setInterval(function(){
+	// 	map.setZoom(0);
+	// 	setTimeout(function(){
+	// 		map.setZoom(1);
+	// 	}, 2000);
+	// }, 4000);
+
 	L.control.layers({
 		'osm': osm.addTo(map),
 		"google": L.tileLayer('http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}', {
 			attribution: 'google'
 		})
 	}, { 'drawlayer': drawnItems }, { position: 'topleft', collapsed: false }).addTo(map);
+
 	map.addControl(new L.Control.Draw({
 		edit: {
 			featureGroup: drawnItems,
@@ -40,7 +50,7 @@ function init() {
 	}));
 
 	map.on(L.Draw.Event.CREATED, function (event) {
-		var layer = event.layer;
+		const layer = event.layer;
 
 		drawnItems.addLayer(layer);
 	});
